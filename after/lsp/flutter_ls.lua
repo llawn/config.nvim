8da9625901535aafe1527e24fa8db25de007e68f
llawn <llawn06@gmail.com>
2026-05-15 17:43:40 +0200

    feat: add lsp config for flutter

---
+++ after/lsp/flutter_ls.lua
@@ -0,0 +1,33 @@
+local flutter_sdk = os.getenv("FLUTTER_ROOT")
+
+if not flutter_sdk then
+  error("FLUTTER_ROOT is not set. Please set it to your Flutter SDK path.")
+end
+
+local analysis_server =
+  flutter_sdk .. "/bin/cache/dart-sdk/bin/snapshots/analysis_server.dart.snapshot"
+
+--- @type vim.lsp.Config
+return {
+  cmd = {
+    "dart",
+    analysis_server,
+    "--lsp",
+  },
+  filetypes = { "dart" },
+  root_markers = { "pubspec.yaml" },
+  init_options = {
+    onlyAnalyzeProjectsWithOpenFiles = true,
+    suggestFromUnimportedLibraries = true,
+    closingLabels = true,
+    outline = true,
+    flutterOutline = true,
+  },
+
+  settings = {
+    dart = {
+      completeFunctionCalls = true,
+      showTodos = true,
+    },
+  },
+}
