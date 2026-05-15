e440d6df1f663bf8b157c5df738104365c1bfc29
llawn <llawn06@gmail.com>
2026-05-15 14:39:22 +0200

    feat: mini.basics setup

---
+++ lua/llawn/plugins/mini/basics.lua
@@ -0,0 +1,20 @@
+-- Mini.basics module for common configuration presets
+local basics = require("mini.basics")
+
+basics.setup({
+  -- Options are manually set in config/options.lua
+  options = { basic = false },
+  mappings = {
+    basic = true,
+    option_toggle_prefix = [[\]],
+    windows = false,
+    move_with_alt = false,
+  },
+  autocommands = {
+    basic = true,
+    relnum_in_visual_mode = false,
+  },
+
+  -- Whether to disable showing non-error feedback
+  silent = false,
+})
